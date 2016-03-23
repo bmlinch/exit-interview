02db02c1df1a7be62a93c2615b9b946933c909f2
bmlinch <bmlinch7266@gmail.com>
2016-03-23 16:08:27 -0600

    Updates with displays and repeats

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 var app = angular.module('app', [])
+
+app.controller('interviewController', function(){
+    var btmdisplay = [];
+    function addingFn(author, url, title){
+        btmdisplay.push(author, url, title)
+        
+    }
+})
